a3ae76ef2dc1a9780e72d7015e6edb021003a0bf
thompsonwyatt <wct2112@columbia.edu>
2017-10-10 19:34:02 -0400

    UI

--- lib/ui.R
+++ lib/ui.R
@@ -82,7 +82,58 @@ dashboardPage(
               
               h2("Introduction"),
               
-              print( "It's a recommendation system for green life in New York. We consider three aspects here: Air quality, Community graden and citibike station....." )
+              tags$h4(type="html", " We care about living a green life, and we have a 
+                     hunch that we’re not the only ones. Global warming 
+                      is real, and driving our cars creates more 
+                      greenhouse gasses that are contributing to the 
+                      problem. Crops are yielding less food, glaciers 
+                      are melting causing the ocean levels to rise, and 
+                      droughts are plaguing areas that once had plenty 
+                      of water. But on an individual level, living a 
+                      green life is important too."),
+              tags$h4("The academic 
+                      literature on the effect of air pollutants on our 
+                      health has grown dramatically in the last decade, 
+                      all pointing in the same direction. We now know 
+                      that breathing pollutants shares a strong 
+                      correlation with small health problems like 
+                      allergies and asthma, and even deathly 
+                      cardiovascular diseases. In fact, the Global 
+                      Burden of Disease has ranked exposure to 
+                      particulate aerosol as the seventh most important 
+                      factor to global mortality. And the National 
+                      Institute of Health has identified a strong 
+                      correlation between those who develop lung cancer 
+                      and those who are exposed to air pollution on a 
+                      daily basis."),
+              tags$h4("Furthermore, these pollutants are 
+                      more present in dense urban areas, and if New York 
+                      City is anything, it’s a dense urban area. This is 
+                      why we’ve made this tool. We want to help you live 
+                      a life in New York City and enjoy the benefits of 
+                      an urban life while avoiding the problems 
+                      associated with breathing bad air. Our tool will 
+                      help you"),
+              
+              tags$h4("•	Explore the distribution of air pollution in New York to help 
+                      you find a place to live with cleaner air \n"
+              ),
+              
+              tags$h4(" •	Show you the location of bike-share programs so you can help 
+                      fight the problem of air pollution by avoiding the harmful fossil-
+                      fuel combustion of motor vehicles \n"),
+              tags$h4("•	Identify community gardens to help find organic, safe produce 
+                      to avoid having to choose between potentially dangerous 
+                      genetically modified organisms (GMO’s) and expensive organic 
+                      alternatives \n"),
+              
+              tags$h4("We believe that living green is important, and we want to empower 
+                      you live green. Enjoy our tool and use it to learn how to live an 
+                      environmentally friendly life. It’s more important now than ever 
+                      to learn about our environmental problems, because as Albert 
+                      Einstein once said, “Problems cannot be solved at the same level 
+                      of awareness that created them.\" " ),
+              HTML('<p><img src="image_nyc.jpg"/></p>')
               
       ),
       
